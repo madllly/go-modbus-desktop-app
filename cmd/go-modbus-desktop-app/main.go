b056c69c39e2c10eaea590da300cfabd897dbc2f
Nikolai Yuminov <mrklen159@gmail.com>
2024-12-22 19:55:46 +0500

    chan in label

--- cmd/go-modbus-desktop-app/main.go
+++ cmd/go-modbus-desktop-app/main.go
@@ -21,8 +21,10 @@ func main() {
 	// w.SetContent(widget.NewLabel("TODOs will go here"))
 	input := widget.NewEntry()
 	input.SetPlaceHolder("Enter text...")
+	channString := make(chan string)
 	content := container.NewVBox(input, widget.NewButton("Save", func() {
 		log.Println("Content was:", input.Text)
+		channString <- input.Text
 		dataStore.SetText(input.Text)
 	}))
 
@@ -30,7 +32,21 @@ func main() {
 	dataStoreGetterContent := container.NewVBox(lable, widget.NewButton("Get From Data Store", func() {
 		lable.SetText(dataStore.GetText())
 		log.Println("Content was:", dataStore.GetText())
+
 	}))
+
+	go func(dataChan <-chan string, lable *widget.Label) {
+		for {
+			data, ok := <-dataChan
+			if !ok {
+				break
+			}
+			log.Println(data)
+			lable.SetText(data)
+		}
+
+	}(channString, lable)
+
 	w.SetContent(container.NewBorder(
 		content,
 		nil,
